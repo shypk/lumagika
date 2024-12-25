d33102f0a55991bbea421da9880a476184b8dfd8
shypk <seohyeok@cscloud.co.jp>
2024-12-25 08:45:55 +0000

    add identify_content for label, content-type

--- src/lib.rs
+++ src/lib.rs
@@ -34,10 +34,35 @@ fn identify_content_label(_: &Lua, content: LuaString) -> LuaResult<String> {
     Ok(ret.to_string())
 }
 
+fn identify_content(_: &Lua, content: LuaString) -> LuaResult<(String, String)> {
+    let ret: &str = "Unknown";
+    return match Session::new() {
+        Ok(magika) => {
+            let result = magika.identify_content_sync(&*content.as_bytes());
+            match result {
+                Ok(result) => {
+                    let info = result.content_type().unwrap_or(ContentType::Unknown).info();
+                    Ok((info.label.to_string(), info.mime_type.to_string()))
+                },
+                Err(e) => {
+                    println!("Error identifying content: {:?}", e);
+                    Ok((ret.to_string(), ret.to_string()))
+                },
+            }
+
+        },
+        Err(e) => {
+            println!("Error identifying content: {:?}", e);
+            Ok((ret.to_string(), ret.to_string()))
+        }
+    };
+}
+
 #[mlua::lua_module]
 fn liblumagika(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("identify_content_label", lua.create_function(identify_content_label)?)?;
+    exports.set("identify_content", lua.create_function(identify_content)?)?;
     Ok(exports)
 }
 
